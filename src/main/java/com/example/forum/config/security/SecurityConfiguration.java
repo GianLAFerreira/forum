defb7c1ca2af3d7364a22b0f9239fd5f00599ed4
Gian Lucas Alves Ferreira <gian.ferreira@mega.com.br>
2022-08-18 20:09:29 -0300

    gerando token

--- src/main/java/com/example/forum/config/security/SecurityConfiguration.java
+++ src/main/java/com/example/forum/config/security/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package com.example.forum.config.security;
 
 import com.example.forum.config.security.service.AutenticacaoService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,6 +18,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Inject
     AutenticacaoService autenticacaoService;
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception{
+        return super.authenticationManager();
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
--- src/main/java/com/example/forum/controller/AutenticacaoController.java
+++ src/main/java/com/example/forum/controller/AutenticacaoController.java
@@ -1,22 +1,45 @@
 package com.example.forum.controller;
 
 import com.example.forum.Form.LoginForm;
+import com.example.forum.config.security.service.TokenService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.inject.Inject;
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
 public class AutenticacaoController {
 
+    @Inject
+    private AuthenticationManager authenticationManager;
+
+    @Inject
+    TokenService tokenService;
+
     @PostMapping
     public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form){
-        System.out.println(form.getEmail() + " " + form.getSenha());
+        UsernamePasswordAuthenticationToken dadosLogin = form.converter();
 
-        return ResponseEntity.ok().build();
+        try {
+            Authentication authentication = authenticationManager.authenticate(dadosLogin);
+
+            String token = tokenService.gerarToken(authentication);
+
+            System.out.println(token);
+
+            return ResponseEntity.ok().build();
+        }catch (AuthenticationException e){
+
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
